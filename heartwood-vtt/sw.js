bc602ef144f5c62d75e797f95e85282df57562e6
hjhudsonwriter <hjhudson.writer@gmail.com>
2026-01-03 16:41:46 +0000

    Update sw.js

--- heartwood-vtt/sw.js
+++ heartwood-vtt/sw.js
@@ -2,12 +2,20 @@ const CACHE = "heartwood-vtt-v2";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./sw.js"];
 
 self.addEventListener("install", (event) => {
-  event.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
+  );
   self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(
+      keys.map((k) => k !== CACHE ? caches.delete(k) : null)
+    );
+    await self.clients.claim();
+  })());
 });
 
 self.addEventListener("fetch", (event) => {
